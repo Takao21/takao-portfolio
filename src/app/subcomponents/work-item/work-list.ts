5d1d571670fddecd9227d8b75417d28e4ff32bae
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-05-01 20:04:15 +0630

    added works descriptions

--- src/app/subcomponents/work-item/work-list.ts
+++ src/app/subcomponents/work-item/work-list.ts
@@ -1,7 +1,7 @@
 export const WORKS = [
   {
     title: "Covid19 Information Webpage",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "Right when the COVID-19 outbreak started, I was practicing Bootstrap 4. So I thought about testing my Bootstrap template skills by creating this informative webpage.",
     imageUrl: "https://lh3.googleusercontent.com/AuUvun5hG8KjT8QfVS1ePsq9k8X7bIeu3LjSDt6Lv1gyCD5Az2JI4yApv2deThvUK23fDltmKdJmRynSVSNkOzgVDLmEWcCRPAgYdxgHBicODc4fHBcWmiU2UIhnnWWXrHFArTOIFgh-RZZZCklxWm9A-qLahPM-SwZU_8CIk_J9tZskt5nChB7nPym052Cu_5B2LVV0_dwnqVqCHiQd-H9EO8sTNzNaZnpf7czUp88mcYGDSy9nUFjLPjzHzEAAyb3UTWRZfKTzJCn8RNMS0yfVQRus83cNtE9qQjZCqdggJAena0cjmCPrRd32RG35fLViZsUCE59rL_SBEQ_cG76XrAQp64WVxRoohWFCNp37mW7HN4SKC-IbOw9RJPQIMd3wPuksOyDK2uicDg5ruiaYhhDWKNwwW3TM-GSwWoQ7-Iitg93dJ7q1KLmjS45JtalKsjmSb-FsziPA53gHNhqHzolcCufu35MnyoU8HAZ9qPKRNruHocKMjfwjbnKw5iIEI0c63_tzjh_Z8-IoC1x5q7alUYyXClXttRnShZryeAT-mUyF0LuDNPZ3Z0q6bb5T1dsWze52g7jk0p1Cz6jvqBClRT9v9M0lMu0uS7EDnR2kLRQpLBxXDChalQR1JQd_PT5lee6lkCupwAiVqoG3ChtyWiD7R46bHMLYayab9HDP0U41neVJIsF3=w1891-h931-no",
     imageCap: "Covid-19 Webpage",
     appUrl: "#",
@@ -9,7 +9,7 @@ export const WORKS = [
   },
   {
     title: "Samsung Stopwatch Remake",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "This is one of my Javascript showcase projects. I took the Samsung Stopwatch as reference and tried to remake it using Javascript. This is a smart-visual responsive stopwatch.",
     imageUrl: "https://lh3.googleusercontent.com/kR_5IxSMjUDOWrn7J4hnkEnJ3YtHL45y9tcnAWVkwy_J_v5WCATcfNYJCDmYY4DccOLtZWUEeVw1hVbJxf3aEqX7J0wsmftX9Xat3GUJ_33JSxGCNhhgLoj98k6z2bAX1zk8hqHVv2PkeUWi9xloOvYlshRn71qEiBq3TV-eXrWEJD8_dW_7UO0Zi82WtURrkktlqZ-bT5k9j6nGOLknyFm5V7IITZJBe_ptvpmUg7vBJ_VvV081ay8CP9nebpTQ2WPXHz_DN9fKxEWot4DuLlbX3OA7F2D3gvZZ2-dc_7i-qsEfbq0o87ubmlm8gVZKwcH2F2-0iKT7yjh1lXYBMU2LV4uHCDsdtHexJvMU5TCD0K7IYpn5BC45l_LNvWnHKCcAxBynmqZdKySTel49dOwWePQOpJtfwMb3RDxFN-btz-BbRofSpkRbtkYRZTlPakQK63NhOFnniUn0NwuVh7IwknIyyfrDwznutnhkBPEyXKOOVCmIx0_c3uCaFDzvaVAs_v7QrgYqkIn69rblL4uEUdMytDZJE9zXEK86X7owAjYWvLZ2wfGMd1pqQEfAPNtRCnuj7UIstQcg4V0vJ_raUnqb_t-2QlfCZPjnz1M5H646dSrvD7TPdZfTxgbfWHEQUTcrtHvItD4ZqVRZtWanNgAaFEDH292h9EG3gleFYob_HE4A8gKBACpS=w1911-h927-no",
     imageCap: "JavaScript Stopwatch",
     appUrl: "#",
@@ -17,7 +17,7 @@ export const WORKS = [
   },
   {
     title: "Morse Code Translator",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "Morse Code is an emergency communication language, introduced as a radio signal language in Titanic. It uses two types of signals to write basic words in critical situations. You can use this app to translate between Morse Code and English. This project mainly proves my Javascript skills and HTML Form Interaction concepts.",
     imageUrl: "https://lh3.googleusercontent.com/fbTe53z62tXx89C3YZfU90iJXhJ3fcVBI7L7BBzIL5WHYg_-8spnIuBXVGQubrvd1xiz35JHdC_WAjG5kOvWL27KkRGieHnppp6ovgjAwm8gc4e96Zos4Z2JrBY3Varfpu3F63vt3H9-LLdTv3TLejT6mSIdO7XL_QFX_463PQodS6FEVnpHY2uLmq1w2wOtWPXIofBZ_vIdbGu7Pwh1XSPCEUtygeZbhsAwmHDYv5MI4HDGn2NhfrN8z2WdIVioL2r4DKxy1Rc6-4Z5spnBMojAh2nyN9V2gTn1wYkWFizCIsolVvfxoU6oRExu9z0klceeJ9O1QnYJ05syRzYri2neeW_ZFmsEMhG9PpapnC_IxW3hnzXvYQofB1IG-nHEydKd7Z9Y4N1aJhsEVvsC_jNGWyjQIjRg0VEspQu6hWjoUIEMMQEpShVKHN4dFxQa5n5GsS5M5oBRMSG9pw_Og6LCssy1hD6OsyJPiNP65S2ZPgwBJdcbt5ZMHEcg475RXg3NS1CB9DA-wVuT0SqsqOqmE6cmaNnBuol4XKaPhIU39Fiun21GGJXI3TkGmXBopT8re3Z63oaNj57EXKVboAo97ILodkychBSp2ncujXM8RMfh1CAt-sBt7Ca8gY_Cf4CzEa9DMTIPg1sR4nhRnD7Ik2cqxttUrVBovgF2huVyaVCH4yWyW6X5Kfpu=w937-h929-no",
     imageCap: "Morse Translator",
     appUrl: "#",
@@ -25,7 +25,7 @@ export const WORKS = [
   },
   {
     title: "YTU Anime Club's Webpage",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "Being the former president of YTU Anime Club, I wanted to contribute something to the club when I retired. So, I made this web project soon after I've learnt AnimeJS framework. You can see the awesome text animation sequence at the start of this webpage.",
     imageUrl: "https://lh3.googleusercontent.com/RV02KdYUGDlO-0XwE2Q9jO0RRxqaS46vLN9Kzq271lTRugHuEVTc8jbgEt6HCXh5xBPfunEs6XCon51usaJ2uv93u-RRa3_ep_7hstNAMdyKpp0-mrIoIDk70rpCHCpJ4356NdjRIXZy0F9IO_TBFlp0O3hAgy2n2odZ6U7buFeQJqnYnJUCu0Irmw-Rm2WxpNCUbR-FJNzU1PoulsAXHGc15tGJJQQ5TKzjPCPwMbLZrB9zfk8jx6xvmqmmHCvxSoV7mTaO0PTmp6Ak7CsL-kjlXQINgjSOXZSriDPtfraz4OMZkODVOvgDdemVo1bvMrjpAcfvuyunwCVVVcOXrpxYIeKjQ-Cpd5g-1dWKrnYeARaLyFNcDtDtTIUrD001z6ATjbY4KEgR1ZGZ75zLCZ_zE3U4SIS3D9eEgPr5A_PZXAqHjsTz__iXVjiLTTLb3llDXpStBMzNfYLSBo7B44ryS7r0qC3wSp7swbTKWiDdwwV8PGXurmlm_vuyYA4PPNyZfl4LKrC_xn5OhSaPOfocDa18gzKirT-lx0KV0DcQzf2pxOwqdJJj6M4e6oZB8LuJFldg_SsYtu6MVFeYObonLoiffIBtP8E1WBt-YBEsZsVKMymxA-FGnpyhbI4Pk5aa023IoxItZL955FIyLSrDizGVaY9f8x4DDEQCnOmx0e5QZUcb6eMzOXIl=w915-h846-no",
     imageCap: "YTU Anime Club",
     appUrl: "#",
@@ -33,7 +33,7 @@ export const WORKS = [
   },
   {
     title: "Voucher Writing App",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "This is one of my proudest projects in my early days of learning web. My father asked me about the Voucher printing apps going around in the local businesses. So, instead of buying those softwares, I wanted to try making one myself since I'm a proud programmer. Thus, using my knowledge of web programming, I created this Voucher Printing Webapp for my dad's shop. It's not an amazing project since it depends on Chrome to run but it will show my dedication and competitive mindset in technology. I felt good being able to help my dad using the technologies I've learnt.",
     imageUrl: "https://lh3.googleusercontent.com/e62U0lv77CH5qEhNHXxoaGB-mbdajO0bXYbLk-FQaFjjRRLKoboIqfrh3GNwifew5VwuMBkd_UvbZLYpGkoj0J01tTaXHdF2_335EumSykB3Xq2DK69TKDl-EsEk2FiKeU6ibcIGQLG2GBUt2hvtkHOK83zVqVcBF-hFgpW_SnW4MPtqMbxGWwkq4tFqKbLhsQJsPoJvFguMOjBV-LIYlWdk43OsvoQIbWvhnHVkdJWYIJoANvfgvk4lW70mqw7lwlzs6dhS4IlcBht9_Wyyh0FEX0dd1-HYRA8dzj4skbnj6E4ib2anNlnrTsN5wjhopWWmEA9U_TV-WKVZo0BM8fltsJgVUPLD-4TIjGMwcWz45D3Zmpji-QhO09huJCcE3rlmthP3jXeOCbB4JwOgpB1K0COqNNGfk1P8W5l23W4UeVlWfJPGHRarpWEHgnUYeakn1lvQXgV34ifRu4SxqGTLEn2RxTPVTa4KJXBwHfP22HfxcR-CugIVcmJkKmz_We4KKQUOfz8lo33LNuRlDWtC1zCVc9gFXeLuF_0-8wxsiTsuRDzXxXavcHNh9_6gDmCdSwcJ9LXm1ByWMCjK-QplyNdwIFsOZotr93NIyuV8SlHFAHupTsuZvPwJh3-7D6hPnbpNBIFVfYwGcFYU1_6qCR8iSxjaIVDlJuP_rs86kl3m6tiYjZM4D9GI=w1898-h934-no",
     imageCap: "Business Voucher",
     appUrl: "#",
@@ -41,7 +41,7 @@ export const WORKS = [
   },
   {
     title: "Write HTML Without Coding",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "A friend of mine was writing repetitive HTML Divs in his school project about a small e-commerce site. And I didn't know about component-based templates and databases at those days so I created him this assisting project. It generates HTML code blocks just by writing what informations you want to add in those blocks. So it basically allows user to write HTML without coding knowledge so people without coding knowledge could help my friend to write his HTML codes.",
     imageUrl: "https://lh3.googleusercontent.com/FqHDCrA-ki5RWFGXTWJNRM4Y074uCSAc75tegLmfUaM2K5rvw51v9NOnuc8MncWslwwMIPihXMFn21nt4s8IAcCQgmhyzruhSAGvD80Xz4zO1o7YXR8A0SmQCRkZm0b3YhBKuWcQJxMx5s37bMRsUcX12qf2cEmuP4_L7gLKCHBUOavW0u4YxtP8qEmVGYh1bdM4UrBnrRuFOBb02CNmIK_gIiIG7ByVz8SZAWtRgP1b8fBYTf38jPBy3Ow_EqyYlb0VKKxYm1Cu5V33gcU6VWDcNB2flKe-1kcVzdVCyJ5SNlFq-_a0xNZH98hbGtRnn2OMnKzc_5dEiUBiGgLPyOldZx6Vlbss-Fl8s0u89m97nr_WKSU1NknrYEseOqeJcWo26d8W0PSlxvs0LZxBie_kXWAB6wMPSzJVdWRTQT6OUHFjSlhzFIKkhAtjIzBXHka3Gqdon7-fiBvHOucu-RY5VfHh47Wiy3e_7n8HUpwT6X7Moh9yqpcABRMyLl7DCekD5XPxSzpCfdXBdfEGZivPjmD0DYewmCCIY6OiTTRXSspKvQWpCXKuFuv6POoW2L16s44W-1kzhGqkEyPPABM_S536JcGAy7ThSiLleME4kOklgL4yvxwbjP6fv_FbY9fEkq60nEvYhDfgzvcA49oBzzdxxT8QqgmSheJARHxqtewHeGsYtggU2_6Y=w1793-h928-no",
     imageCap: "HTML Generator",
     appUrl: "#",
@@ -49,7 +49,7 @@ export const WORKS = [
   },
   {
     title: "Product Landing Page Design",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "This project was one of my Freecodecamp's Certification Projects. It is a responsive product selling page, featuring an email reception, the provided services, products for sale and an advertising video. The logo image link might be dead cuz it was uploaded temporarily for the Certification project.",
     imageUrl: "https://lh3.googleusercontent.com/dPV1m-PNSEn2v1lVmjht88FtkxUnUI1lFxE_VJgRAZ1t4x-PbYWkpbHWCF_hrU-2qKjfjcA7i7H2iOX07l5It2gqZG7F4j601sxZBsdBWSdWkwQpjhYdRXbafbE4a_J3XGV1jHWHo2J--eTj5xXJyTSREod2nO5Ru06cTxThU0Edc28decMecTqkI3cHj2Jq7MJ3NLnb99MauHNl_SJlQ8fOl9lKi8jw3QdWbbcbdzgMm3yY6BnG0a8zVO3ZJZ9yb-o0cj4iPD7vrtgmUKsx5LNQ054o4NdKWHvEC8LqNULBVQ2PO0lVJ7nTwr7Hqp3KyXw22vJuM-6eNZPfb0lLRa-7UYRQEPU-JNR8Z-rbLpENqZtZ8EdLme-Zwq1vVh1DQj9Jo2EydlNzbGy9TaRbKS3_6BaKcIR7oUAoYm2QHPExXwmMQMTqTbuwr2JNeULuqdeKnOPAN_R1Co5_9k4Es1mJDWLic6qf126mNqQnprxapJx5d0Sr5yApcUB7tU3rZU4LdHD4sg6BEwhQhosqlqblDqmKPt832T19eeCdwpUdHyWsAXbYaRlqgezBOcoQzjkMb-Oedl_ieVIBjhb9H95robVOdnsouiA8lUl0Bgdbt8TnSUKPoQjlP6lDQBBoixvlZUAZcZWaCss1xHSi6bsNO_e9G6QcKrwq_NKlrHFaBTCQvRFUPZlMhdxI=w1609-h917-no",
     imageCap: "Product Sale",
     appUrl: "#",
@@ -57,7 +57,7 @@ export const WORKS = [
   },
   {
     title: "Educational Document Page",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "I tried to duplicate an education website that features Javascript documentations. This project is to showcase my responsive layout designing knowledge for documentations, online libraries and news articles.",
     imageUrl: "https://lh3.googleusercontent.com/NU0SterjACC0AQ4SQn_xBh-BB83IgEAgJdDZcP8yxtKt5qYpxfjDCxlDjFOA14doLZ9OvIHVziz4oA-TuEi87Zuax1V-XdTuzotGI7N_QTWMykSZJyIUWpXjfse1o1PrQI4FMPU4VUwXtoKGxzhSPNVGdUxYgQcTPxJ9vtDw5psTdFmZoxoGrJ3j98Pk16Jm7XnXnhQFldGZkPr67qu-rLBzxnNsZpXjbq8fxlT9buTvNkjVEYRmJFyPy1Zu38RROaKMLc6H9fBATYp4hZW2qHL3P7SXvfiMsSrZwxSYrrxMuZqPvVqFlyU_scTnlopYOft3dA4MEovYxSFbD8KO0Df5DSoV1nuyfWn6_vfPtoF1LWV3o-gH0ELjhPUYcaOrJcTEKAU6PUmZuCNp_6fh8OXOmqd1IeE0Xfv8DNJdKjyUpgOrhdAC4a0on1MuAPi0bY1zsOG4Qzo47V4RqkcVevMapXgdQlK7gWsqcE2CM6lfKH7n65ca1K1W6HPr3hkA8BVSHSUJBftr0tJvqMtksxCdgooww3nmRYB6gBRIs-Lrh4vvpbNIF1mvbzRG9h3sPsuWGxi-FsLtZLlptNJo3dPT-0kkPg-V9G6VcPYnfuxS1ZLliTJKg5oiphPslloTrkrp4LFlg3B5qBFR5me2xIdxfoG46G3KqjFxrSmWXgV8VzF_DzTXFxg54Kli=w1523-h934-no",
     imageCap: "Documentation Page",
     appUrl: "#",
@@ -65,7 +65,7 @@ export const WORKS = [
   },
   {
     title: "Angular Calculator",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "An angular portfolio project. It's a simple calculator making use of Angular's data binding properties. This small project might seem underwhelming to show Angular skills but, Voila! This whole Portfolio Website itself is written in Angular. :)",
     imageUrl: "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg",
     imageCap: "Simple Calculator",
     appUrl: "#",
