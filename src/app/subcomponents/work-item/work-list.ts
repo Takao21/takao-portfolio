8318d1d3db0c6e1c5b85fe952c4434a3913da0b2
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-05-04 20:27:20 +0630

    fixed images not loading bug

--- src/app/subcomponents/work-item/work-list.ts
+++ src/app/subcomponents/work-item/work-list.ts
@@ -2,7 +2,7 @@ export const WORKS = [
   {
     title: "Covid19 Information Webpage",
     description: "Right when the COVID-19 outbreak started, I was practicing Bootstrap 4. So I thought about testing my Bootstrap template skills by creating this informative webpage.",
-    imageUrl: "https://lh3.googleusercontent.com/AuUvun5hG8KjT8QfVS1ePsq9k8X7bIeu3LjSDt6Lv1gyCD5Az2JI4yApv2deThvUK23fDltmKdJmRynSVSNkOzgVDLmEWcCRPAgYdxgHBicODc4fHBcWmiU2UIhnnWWXrHFArTOIFgh-RZZZCklxWm9A-qLahPM-SwZU_8CIk_J9tZskt5nChB7nPym052Cu_5B2LVV0_dwnqVqCHiQd-H9EO8sTNzNaZnpf7czUp88mcYGDSy9nUFjLPjzHzEAAyb3UTWRZfKTzJCn8RNMS0yfVQRus83cNtE9qQjZCqdggJAena0cjmCPrRd32RG35fLViZsUCE59rL_SBEQ_cG76XrAQp64WVxRoohWFCNp37mW7HN4SKC-IbOw9RJPQIMd3wPuksOyDK2uicDg5ruiaYhhDWKNwwW3TM-GSwWoQ7-Iitg93dJ7q1KLmjS45JtalKsjmSb-FsziPA53gHNhqHzolcCufu35MnyoU8HAZ9qPKRNruHocKMjfwjbnKw5iIEI0c63_tzjh_Z8-IoC1x5q7alUYyXClXttRnShZryeAT-mUyF0LuDNPZ3Z0q6bb5T1dsWze52g7jk0p1Cz6jvqBClRT9v9M0lMu0uS7EDnR2kLRQpLBxXDChalQR1JQd_PT5lee6lkCupwAiVqoG3ChtyWiD7R46bHMLYayab9HDP0U41neVJIsF3=w1891-h931-no",
+    imageUrl: "https://lh3.googleusercontent.com/CDn-ZMpgu99Zx7pe4tWjfCRXwbb9mKms41rgd4WHiu160iWouUVZlRrovSZqO4za5WDhuuj-BoXvliNeonSKI0AjZIKbm1RtPgN5JAzAzStd2JEiE3y9DoS1cGY9kWzNeV01icfnyA=w2400",
     imageCap: "Covid-19 Webpage",
     appUrl: "https://takao21.github.io/Bootstrap-corona/",
     sourceUrl: "https://github.com/Takao21/Bootstrap-corona"
@@ -10,7 +10,7 @@ export const WORKS = [
   {
     title: "Samsung Stopwatch Remake",
     description: "This is one of my Javascript showcase projects. I took the Samsung Stopwatch as reference and tried to remake it using Javascript. This is a smart-visual responsive stopwatch.",
-    imageUrl: "https://lh3.googleusercontent.com/kR_5IxSMjUDOWrn7J4hnkEnJ3YtHL45y9tcnAWVkwy_J_v5WCATcfNYJCDmYY4DccOLtZWUEeVw1hVbJxf3aEqX7J0wsmftX9Xat3GUJ_33JSxGCNhhgLoj98k6z2bAX1zk8hqHVv2PkeUWi9xloOvYlshRn71qEiBq3TV-eXrWEJD8_dW_7UO0Zi82WtURrkktlqZ-bT5k9j6nGOLknyFm5V7IITZJBe_ptvpmUg7vBJ_VvV081ay8CP9nebpTQ2WPXHz_DN9fKxEWot4DuLlbX3OA7F2D3gvZZ2-dc_7i-qsEfbq0o87ubmlm8gVZKwcH2F2-0iKT7yjh1lXYBMU2LV4uHCDsdtHexJvMU5TCD0K7IYpn5BC45l_LNvWnHKCcAxBynmqZdKySTel49dOwWePQOpJtfwMb3RDxFN-btz-BbRofSpkRbtkYRZTlPakQK63NhOFnniUn0NwuVh7IwknIyyfrDwznutnhkBPEyXKOOVCmIx0_c3uCaFDzvaVAs_v7QrgYqkIn69rblL4uEUdMytDZJE9zXEK86X7owAjYWvLZ2wfGMd1pqQEfAPNtRCnuj7UIstQcg4V0vJ_raUnqb_t-2QlfCZPjnz1M5H646dSrvD7TPdZfTxgbfWHEQUTcrtHvItD4ZqVRZtWanNgAaFEDH292h9EG3gleFYob_HE4A8gKBACpS=w1911-h927-no",
+    imageUrl: "https://lh3.googleusercontent.com/2q6SHuoBa_KdBRgDZu77ogaEISZ7W9JJaYFF9LXNCHWBliFQhDg0oBh0b02ofuSK6wgIGg7skWXWyT8d7NHb2Qt6LokpDtbjv3Wps0vDdsZU1nSqwvYpPIYS7mLpoDfW11n_joPjgg=w2400",
     imageCap: "JavaScript Stopwatch",
     appUrl: "https://codepen.io/takao21/full/oNjzOeO",
     sourceUrl: "https://github.com/Takao21/stopwatch-js"
@@ -18,7 +18,7 @@ export const WORKS = [
   {
     title: "Morse Code Translator",
     description: "Morse Code is an emergency communication language, introduced as a radio signal language in Titanic. It uses two types of signals to write basic words in critical situations. You can use this app to translate between Morse Code and English. This project mainly proves my Javascript skills and HTML Form Interaction concepts.",
-    imageUrl: "https://lh3.googleusercontent.com/fbTe53z62tXx89C3YZfU90iJXhJ3fcVBI7L7BBzIL5WHYg_-8spnIuBXVGQubrvd1xiz35JHdC_WAjG5kOvWL27KkRGieHnppp6ovgjAwm8gc4e96Zos4Z2JrBY3Varfpu3F63vt3H9-LLdTv3TLejT6mSIdO7XL_QFX_463PQodS6FEVnpHY2uLmq1w2wOtWPXIofBZ_vIdbGu7Pwh1XSPCEUtygeZbhsAwmHDYv5MI4HDGn2NhfrN8z2WdIVioL2r4DKxy1Rc6-4Z5spnBMojAh2nyN9V2gTn1wYkWFizCIsolVvfxoU6oRExu9z0klceeJ9O1QnYJ05syRzYri2neeW_ZFmsEMhG9PpapnC_IxW3hnzXvYQofB1IG-nHEydKd7Z9Y4N1aJhsEVvsC_jNGWyjQIjRg0VEspQu6hWjoUIEMMQEpShVKHN4dFxQa5n5GsS5M5oBRMSG9pw_Og6LCssy1hD6OsyJPiNP65S2ZPgwBJdcbt5ZMHEcg475RXg3NS1CB9DA-wVuT0SqsqOqmE6cmaNnBuol4XKaPhIU39Fiun21GGJXI3TkGmXBopT8re3Z63oaNj57EXKVboAo97ILodkychBSp2ncujXM8RMfh1CAt-sBt7Ca8gY_Cf4CzEa9DMTIPg1sR4nhRnD7Ik2cqxttUrVBovgF2huVyaVCH4yWyW6X5Kfpu=w937-h929-no",
+    imageUrl: "https://lh3.googleusercontent.com/xZpS2clWT0oZWMt2heiuig6gwGLIKWiXsJ7xkWWvypUsYt1CM9PxYgC0w8379wOHygc_gvlnRUoRcmIgOqM5OzceqL_d3EE-HdpYLWda9uRF50XNkFthsariwtiN3I1SNCO6sscb7w=w2400",
     imageCap: "Morse Translator",
     appUrl: "https://codepen.io/takao21/full/ExVjpra",
     sourceUrl: "https://github.com/Takao21/morse-translator"
@@ -26,7 +26,7 @@ export const WORKS = [
   {
     title: "YTU Anime Club's Webpage",
     description: "Being the former president of YTU Anime Club, I wanted to contribute something to the club when I retired. So, I made this web project soon after I've learnt AnimeJS framework. You can see the awesome text animation sequence at the start of this webpage.",
-    imageUrl: "https://lh3.googleusercontent.com/RV02KdYUGDlO-0XwE2Q9jO0RRxqaS46vLN9Kzq271lTRugHuEVTc8jbgEt6HCXh5xBPfunEs6XCon51usaJ2uv93u-RRa3_ep_7hstNAMdyKpp0-mrIoIDk70rpCHCpJ4356NdjRIXZy0F9IO_TBFlp0O3hAgy2n2odZ6U7buFeQJqnYnJUCu0Irmw-Rm2WxpNCUbR-FJNzU1PoulsAXHGc15tGJJQQ5TKzjPCPwMbLZrB9zfk8jx6xvmqmmHCvxSoV7mTaO0PTmp6Ak7CsL-kjlXQINgjSOXZSriDPtfraz4OMZkODVOvgDdemVo1bvMrjpAcfvuyunwCVVVcOXrpxYIeKjQ-Cpd5g-1dWKrnYeARaLyFNcDtDtTIUrD001z6ATjbY4KEgR1ZGZ75zLCZ_zE3U4SIS3D9eEgPr5A_PZXAqHjsTz__iXVjiLTTLb3llDXpStBMzNfYLSBo7B44ryS7r0qC3wSp7swbTKWiDdwwV8PGXurmlm_vuyYA4PPNyZfl4LKrC_xn5OhSaPOfocDa18gzKirT-lx0KV0DcQzf2pxOwqdJJj6M4e6oZB8LuJFldg_SsYtu6MVFeYObonLoiffIBtP8E1WBt-YBEsZsVKMymxA-FGnpyhbI4Pk5aa023IoxItZL955FIyLSrDizGVaY9f8x4DDEQCnOmx0e5QZUcb6eMzOXIl=w915-h846-no",
+    imageUrl: "https://lh3.googleusercontent.com/AhHpWWoHDUHVR8PwGiZeBl7dZ8Fvbp40FOqvvzBynn2d4C5ezAkd4GB8-1bse05VeTIl8VvUOFS-D2Q8YzHapezOnn2q8xCIGgttCuOOMvhPiNuFoGmAv3L9JTSpLQIZ0Bj-xEu8cQ=w2400",
     imageCap: "YTU Anime Club",
     appUrl: "https://takao21.github.io/ytuanimeclub-webpage/",
     sourceUrl: "https://github.com/Takao21/ytuanimeclub-webpage/"
@@ -34,7 +34,7 @@ export const WORKS = [
   {
     title: "Voucher Writing App",
     description: "This is one of my proudest projects in my early days of learning web. My father asked me about the Voucher printing apps going around in the local businesses. So, instead of buying those softwares, I wanted to try making one myself since I'm a proud programmer. Thus, using my knowledge of web programming, I created this Voucher Printing Webapp for my dad's shop. It's not an amazing project since it depends on Chrome to run but it will show my dedication and competitive mindset in technology. I felt good being able to help my dad using the technologies I've learnt.",
-    imageUrl: "https://lh3.googleusercontent.com/e62U0lv77CH5qEhNHXxoaGB-mbdajO0bXYbLk-FQaFjjRRLKoboIqfrh3GNwifew5VwuMBkd_UvbZLYpGkoj0J01tTaXHdF2_335EumSykB3Xq2DK69TKDl-EsEk2FiKeU6ibcIGQLG2GBUt2hvtkHOK83zVqVcBF-hFgpW_SnW4MPtqMbxGWwkq4tFqKbLhsQJsPoJvFguMOjBV-LIYlWdk43OsvoQIbWvhnHVkdJWYIJoANvfgvk4lW70mqw7lwlzs6dhS4IlcBht9_Wyyh0FEX0dd1-HYRA8dzj4skbnj6E4ib2anNlnrTsN5wjhopWWmEA9U_TV-WKVZo0BM8fltsJgVUPLD-4TIjGMwcWz45D3Zmpji-QhO09huJCcE3rlmthP3jXeOCbB4JwOgpB1K0COqNNGfk1P8W5l23W4UeVlWfJPGHRarpWEHgnUYeakn1lvQXgV34ifRu4SxqGTLEn2RxTPVTa4KJXBwHfP22HfxcR-CugIVcmJkKmz_We4KKQUOfz8lo33LNuRlDWtC1zCVc9gFXeLuF_0-8wxsiTsuRDzXxXavcHNh9_6gDmCdSwcJ9LXm1ByWMCjK-QplyNdwIFsOZotr93NIyuV8SlHFAHupTsuZvPwJh3-7D6hPnbpNBIFVfYwGcFYU1_6qCR8iSxjaIVDlJuP_rs86kl3m6tiYjZM4D9GI=w1898-h934-no",
+    imageUrl: "https://lh3.googleusercontent.com/D55fvy8yF5BdmHzJWv9qc8EYBSgIf3wNH-4FH2MJI2EW1oDcvWL9VWycmvtBOY7Vri1He3YBZXFdxzzWGR-ti_dRApsb2H9oWDtT7IeVJtZrOaHaTcIO-yv4d15GvBmsnCDCxprx8w=w2400",
     imageCap: "Business Voucher",
     appUrl: "https://takao21.github.io/voucher-app/",
     sourceUrl: "https://github.com/Takao21/voucher-app/"
@@ -50,7 +50,7 @@ export const WORKS = [
   {
     title: "Product Landing Design",
     description: "This project was one of my Freecodecamp's Certification Projects. It is a responsive product selling page, featuring an email reception, the provided services, products for sale and an advertising video. The logo image link might be dead cuz it was uploaded temporarily for the Certification project.",
-    imageUrl: "https://lh3.googleusercontent.com/dPV1m-PNSEn2v1lVmjht88FtkxUnUI1lFxE_VJgRAZ1t4x-PbYWkpbHWCF_hrU-2qKjfjcA7i7H2iOX07l5It2gqZG7F4j601sxZBsdBWSdWkwQpjhYdRXbafbE4a_J3XGV1jHWHo2J--eTj5xXJyTSREod2nO5Ru06cTxThU0Edc28decMecTqkI3cHj2Jq7MJ3NLnb99MauHNl_SJlQ8fOl9lKi8jw3QdWbbcbdzgMm3yY6BnG0a8zVO3ZJZ9yb-o0cj4iPD7vrtgmUKsx5LNQ054o4NdKWHvEC8LqNULBVQ2PO0lVJ7nTwr7Hqp3KyXw22vJuM-6eNZPfb0lLRa-7UYRQEPU-JNR8Z-rbLpENqZtZ8EdLme-Zwq1vVh1DQj9Jo2EydlNzbGy9TaRbKS3_6BaKcIR7oUAoYm2QHPExXwmMQMTqTbuwr2JNeULuqdeKnOPAN_R1Co5_9k4Es1mJDWLic6qf126mNqQnprxapJx5d0Sr5yApcUB7tU3rZU4LdHD4sg6BEwhQhosqlqblDqmKPt832T19eeCdwpUdHyWsAXbYaRlqgezBOcoQzjkMb-Oedl_ieVIBjhb9H95robVOdnsouiA8lUl0Bgdbt8TnSUKPoQjlP6lDQBBoixvlZUAZcZWaCss1xHSi6bsNO_e9G6QcKrwq_NKlrHFaBTCQvRFUPZlMhdxI=w1609-h917-no",
+    imageUrl: "https://lh3.googleusercontent.com/m0-kpqUGgr4jWIz_4S1nqILz6utkkV2AIvgFV6GpKpNwifMi-sb5Fgser9cxHFjnXem0turjtiLxGxKIKphTEyO0V4OGtCD4nSEsgDdDf6Dm3zJtAvqJHLvhcKWFi3fskK07l2sJMg=w2400",
     imageCap: "Product Sale",
     appUrl: "https://codepen.io/takao21/full/BaowJbx",
     sourceUrl: "https://github.com/Takao21/freecodecamp-landing-page"
@@ -58,7 +58,7 @@ export const WORKS = [
   {
     title: "Educational Document Page",
     description: "I tried to duplicate an education website that features Javascript documentations. This project is to showcase my responsive layout designing knowledge for documentations, online libraries and news articles.",
-    imageUrl: "https://lh3.googleusercontent.com/NU0SterjACC0AQ4SQn_xBh-BB83IgEAgJdDZcP8yxtKt5qYpxfjDCxlDjFOA14doLZ9OvIHVziz4oA-TuEi87Zuax1V-XdTuzotGI7N_QTWMykSZJyIUWpXjfse1o1PrQI4FMPU4VUwXtoKGxzhSPNVGdUxYgQcTPxJ9vtDw5psTdFmZoxoGrJ3j98Pk16Jm7XnXnhQFldGZkPr67qu-rLBzxnNsZpXjbq8fxlT9buTvNkjVEYRmJFyPy1Zu38RROaKMLc6H9fBATYp4hZW2qHL3P7SXvfiMsSrZwxSYrrxMuZqPvVqFlyU_scTnlopYOft3dA4MEovYxSFbD8KO0Df5DSoV1nuyfWn6_vfPtoF1LWV3o-gH0ELjhPUYcaOrJcTEKAU6PUmZuCNp_6fh8OXOmqd1IeE0Xfv8DNJdKjyUpgOrhdAC4a0on1MuAPi0bY1zsOG4Qzo47V4RqkcVevMapXgdQlK7gWsqcE2CM6lfKH7n65ca1K1W6HPr3hkA8BVSHSUJBftr0tJvqMtksxCdgooww3nmRYB6gBRIs-Lrh4vvpbNIF1mvbzRG9h3sPsuWGxi-FsLtZLlptNJo3dPT-0kkPg-V9G6VcPYnfuxS1ZLliTJKg5oiphPslloTrkrp4LFlg3B5qBFR5me2xIdxfoG46G3KqjFxrSmWXgV8VzF_DzTXFxg54Kli=w1523-h934-no",
+    imageUrl: "https://lh3.googleusercontent.com/Sa5e8J7wwhUfjA9dZkCKZUx-prPPDMFerjoAgBPdCTEw04PKvsLOtAfHW4OkCSUq5HZCoHpso7J_NPmnz69hUo8bmPPBKRiC3UugSZNlqCuWRRBSBYfJAqI23t1nbsOGT6IM0eufDg=w2400",
     imageCap: "Documentation Page",
     appUrl: "https://codepen.io/takao21/full/BaowJgV",
     sourceUrl: "https://github.com/Takao21/freecodecamp-techdoc-page"
@@ -66,7 +66,7 @@ export const WORKS = [
   {
     title: "Angular Calculator",
     description: "An angular portfolio project. It's a simple calculator making use of Angular's data binding properties. This small project might seem underwhelming to show Angular skills but, Voila! This whole Portfolio Website itself is written in Angular. :)",
-    imageUrl: "https://lh3.googleusercontent.com/MJ4Ey-ZFri2pRTWcO0bVs8rnu_tktziWR2Xru4rafEePrFokyfyiO7u8L78qJGKqLCdYJ18AzSvUmYRAdMJvNBS6OKQrXqbJACEr2W8Zm6PB70ka8msyF1ZRWMGAAmRnwPhx8OzMOSynBbgeoTljrBeskuWXK13_KJyUQkPaVqkLpyXALOKbPBEdj1TIWKIbjo2Pn8-wmnuHv0CzIjHTP-GTyGi9UTJku2huJVCZciSrEH_qFGAu0f3ZflnCadDZx_RY8IY8JW3IqHsFB1-VQyEXbYdhev5uL1tKlxW6CSEyf_Indr-IheO4mT0-L9mWtrBjZ0d7w3pB5M4ieAd6vUynWuvOzk0rEtPqHds7xw_rxOOaqZQ6WaqGQXLmsH5eCGSUe-9VCohGRyicaqJbsgaoQwfzdCc5EeYPSTtKgb-eGC00rS1q1bzxXB1Zg0_fBukw1uMixunItJkmzkW6RR6gy-4ZNnxuXL6MP3GPA94V_YSJtEyDCKlToKJUe8hFrwZjYIE3DBWLz3Jq0IPc4LjQOi_nTz7j5H8f8gvZXDiD8UfZmkSms0MDDhkyfBW-4TFvrm6sJ__RRDqK1MqOkFbxH9Kp1uNtrDHGNGeVLHdHPlT2ZBqI71cuxbLRxzX16qReqfJ-86Rb6MgKiBKWSVgJ2s1GNIhv_5jtSzSHcleUDT4EKr-Li9seNMrf=w1015-h883-no",
+    imageUrl: "https://lh3.googleusercontent.com/HetEEqeEgETOnNeGhnSmhB7S_i4OwhbB_btfxQDTRSC1SjXdEMTUQi7AO9cow2D5Goj3ew0ULuR1-ZTb73gyS46Ujbl7oi_2zOJ-x9GYhO6wACnpcq-M_ezUwRshh5zxuiO5TEYMYw=w2400",
     imageCap: "Simple Calculator",
     appUrl: "https://takao21.github.io/ng-calculator/",
     sourceUrl: "https://github.com/Takao21/ng-calculator"
