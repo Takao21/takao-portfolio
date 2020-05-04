92b29293c33026f23e77c5d33525edb305f8ca15
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-05-04 20:51:01 +0630

    fixed image bug

--- src/app/subcomponents/work-item/work-list.ts
+++ src/app/subcomponents/work-item/work-list.ts
@@ -2,7 +2,7 @@ export const WORKS = [
   {
     title: "Covid19 Information Webpage",
     description: "Right when the COVID-19 outbreak started, I was practicing Bootstrap 4. So I thought about testing my Bootstrap template skills by creating this informative webpage.",
-    imageUrl: "https://lh3.googleusercontent.com/CDn-ZMpgu99Zx7pe4tWjfCRXwbb9mKms41rgd4WHiu160iWouUVZlRrovSZqO4za5WDhuuj-BoXvliNeonSKI0AjZIKbm1RtPgN5JAzAzStd2JEiE3y9DoS1cGY9kWzNeV01icfnyA=w2400",
+    imageUrl: "https://i.imgur.com/aPhSVib.png",
     imageCap: "Covid-19 Webpage",
     appUrl: "https://takao21.github.io/Bootstrap-corona/",
     sourceUrl: "https://github.com/Takao21/Bootstrap-corona"
@@ -10,7 +10,7 @@ export const WORKS = [
   {
     title: "Samsung Stopwatch Remake",
     description: "This is one of my Javascript showcase projects. I took the Samsung Stopwatch as reference and tried to remake it using Javascript. This is a smart-visual responsive stopwatch.",
-    imageUrl: "https://lh3.googleusercontent.com/2q6SHuoBa_KdBRgDZu77ogaEISZ7W9JJaYFF9LXNCHWBliFQhDg0oBh0b02ofuSK6wgIGg7skWXWyT8d7NHb2Qt6LokpDtbjv3Wps0vDdsZU1nSqwvYpPIYS7mLpoDfW11n_joPjgg=w2400",
+    imageUrl: "https://i.imgur.com/SJNtLXp.png",
     imageCap: "JavaScript Stopwatch",
     appUrl: "https://codepen.io/takao21/full/oNjzOeO",
     sourceUrl: "https://github.com/Takao21/stopwatch-js"
@@ -18,7 +18,7 @@ export const WORKS = [
   {
     title: "Morse Code Translator",
     description: "Morse Code is an emergency communication language, introduced as a radio signal language in Titanic. It uses two types of signals to write basic words in critical situations. You can use this app to translate between Morse Code and English. This project mainly proves my Javascript skills and HTML Form Interaction concepts.",
-    imageUrl: "https://lh3.googleusercontent.com/xZpS2clWT0oZWMt2heiuig6gwGLIKWiXsJ7xkWWvypUsYt1CM9PxYgC0w8379wOHygc_gvlnRUoRcmIgOqM5OzceqL_d3EE-HdpYLWda9uRF50XNkFthsariwtiN3I1SNCO6sscb7w=w2400",
+    imageUrl: "https://i.imgur.com/RF2BSNz.png",
     imageCap: "Morse Translator",
     appUrl: "https://codepen.io/takao21/full/ExVjpra",
     sourceUrl: "https://github.com/Takao21/morse-translator"
@@ -26,7 +26,7 @@ export const WORKS = [
   {
     title: "YTU Anime Club's Webpage",
     description: "Being the former president of YTU Anime Club, I wanted to contribute something to the club when I retired. So, I made this web project soon after I've learnt AnimeJS framework. You can see the awesome text animation sequence at the start of this webpage.",
-    imageUrl: "https://lh3.googleusercontent.com/AhHpWWoHDUHVR8PwGiZeBl7dZ8Fvbp40FOqvvzBynn2d4C5ezAkd4GB8-1bse05VeTIl8VvUOFS-D2Q8YzHapezOnn2q8xCIGgttCuOOMvhPiNuFoGmAv3L9JTSpLQIZ0Bj-xEu8cQ=w2400",
+    imageUrl: "https://i.imgur.com/JgHNw5R.png",
     imageCap: "YTU Anime Club",
     appUrl: "https://takao21.github.io/ytuanimeclub-webpage/",
     sourceUrl: "https://github.com/Takao21/ytuanimeclub-webpage/"
@@ -34,7 +34,7 @@ export const WORKS = [
   {
     title: "Voucher Writing App",
     description: "This is one of my proudest projects in my early days of learning web. My father asked me about the Voucher printing apps going around in the local businesses. So, instead of buying those softwares, I wanted to try making one myself since I'm a proud programmer. Thus, using my knowledge of web programming, I created this Voucher Printing Webapp for my dad's shop. It's not an amazing project since it depends on Chrome to run but it will show my dedication and competitive mindset in technology. I felt good being able to help my dad using the technologies I've learnt.",
-    imageUrl: "https://lh3.googleusercontent.com/D55fvy8yF5BdmHzJWv9qc8EYBSgIf3wNH-4FH2MJI2EW1oDcvWL9VWycmvtBOY7Vri1He3YBZXFdxzzWGR-ti_dRApsb2H9oWDtT7IeVJtZrOaHaTcIO-yv4d15GvBmsnCDCxprx8w=w2400",
+    imageUrl: "https://i.imgur.com/fE0qF4o.png",
     imageCap: "Business Voucher",
     appUrl: "https://takao21.github.io/voucher-app/",
     sourceUrl: "https://github.com/Takao21/voucher-app/"
@@ -42,7 +42,7 @@ export const WORKS = [
   {
     title: "Write HTML Without Coding",
     description: "A friend of mine was writing repetitive HTML Divs in his school project about a small e-commerce site. And I didn't know about component-based templates and databases at those days so I created him this assisting project. It generates HTML code blocks just by writing what informations you want to add in those blocks. So it basically allows user to write HTML without coding knowledge so people without coding knowledge could help my friend to write his HTML codes.",
-    imageUrl: "https://lh3.googleusercontent.com/FqHDCrA-ki5RWFGXTWJNRM4Y074uCSAc75tegLmfUaM2K5rvw51v9NOnuc8MncWslwwMIPihXMFn21nt4s8IAcCQgmhyzruhSAGvD80Xz4zO1o7YXR8A0SmQCRkZm0b3YhBKuWcQJxMx5s37bMRsUcX12qf2cEmuP4_L7gLKCHBUOavW0u4YxtP8qEmVGYh1bdM4UrBnrRuFOBb02CNmIK_gIiIG7ByVz8SZAWtRgP1b8fBYTf38jPBy3Ow_EqyYlb0VKKxYm1Cu5V33gcU6VWDcNB2flKe-1kcVzdVCyJ5SNlFq-_a0xNZH98hbGtRnn2OMnKzc_5dEiUBiGgLPyOldZx6Vlbss-Fl8s0u89m97nr_WKSU1NknrYEseOqeJcWo26d8W0PSlxvs0LZxBie_kXWAB6wMPSzJVdWRTQT6OUHFjSlhzFIKkhAtjIzBXHka3Gqdon7-fiBvHOucu-RY5VfHh47Wiy3e_7n8HUpwT6X7Moh9yqpcABRMyLl7DCekD5XPxSzpCfdXBdfEGZivPjmD0DYewmCCIY6OiTTRXSspKvQWpCXKuFuv6POoW2L16s44W-1kzhGqkEyPPABM_S536JcGAy7ThSiLleME4kOklgL4yvxwbjP6fv_FbY9fEkq60nEvYhDfgzvcA49oBzzdxxT8QqgmSheJARHxqtewHeGsYtggU2_6Y=w1793-h928-no",
+    imageUrl: "https://i.imgur.com/qywbq6T.png",
     imageCap: "HTML Generator",
     appUrl: "https://takao21.github.io/code-generator-test/",
     sourceUrl: "https://github.com/Takao21/code-generator-test/"
@@ -50,7 +50,7 @@ export const WORKS = [
   {
     title: "Product Landing Design",
     description: "This project was one of my Freecodecamp's Certification Projects. It is a responsive product selling page, featuring an email reception, the provided services, products for sale and an advertising video. The logo image link might be dead cuz it was uploaded temporarily for the Certification project.",
-    imageUrl: "https://lh3.googleusercontent.com/m0-kpqUGgr4jWIz_4S1nqILz6utkkV2AIvgFV6GpKpNwifMi-sb5Fgser9cxHFjnXem0turjtiLxGxKIKphTEyO0V4OGtCD4nSEsgDdDf6Dm3zJtAvqJHLvhcKWFi3fskK07l2sJMg=w2400",
+    imageUrl: "https://i.imgur.com/pVsCOx6.png",
     imageCap: "Product Sale",
     appUrl: "https://codepen.io/takao21/full/BaowJbx",
     sourceUrl: "https://github.com/Takao21/freecodecamp-landing-page"
@@ -58,7 +58,7 @@ export const WORKS = [
   {
     title: "Educational Document Page",
     description: "I tried to duplicate an education website that features Javascript documentations. This project is to showcase my responsive layout designing knowledge for documentations, online libraries and news articles.",
-    imageUrl: "https://lh3.googleusercontent.com/Sa5e8J7wwhUfjA9dZkCKZUx-prPPDMFerjoAgBPdCTEw04PKvsLOtAfHW4OkCSUq5HZCoHpso7J_NPmnz69hUo8bmPPBKRiC3UugSZNlqCuWRRBSBYfJAqI23t1nbsOGT6IM0eufDg=w2400",
+    imageUrl: "https://i.imgur.com/qywbq6T.png",
     imageCap: "Documentation Page",
     appUrl: "https://codepen.io/takao21/full/BaowJgV",
     sourceUrl: "https://github.com/Takao21/freecodecamp-techdoc-page"
@@ -66,7 +66,7 @@ export const WORKS = [
   {
     title: "Angular Calculator",
     description: "An angular portfolio project. It's a simple calculator making use of Angular's data binding properties. This small project might seem underwhelming to show Angular skills but, Voila! This whole Portfolio Website itself is written in Angular. :)",
-    imageUrl: "https://lh3.googleusercontent.com/HetEEqeEgETOnNeGhnSmhB7S_i4OwhbB_btfxQDTRSC1SjXdEMTUQi7AO9cow2D5Goj3ew0ULuR1-ZTb73gyS46Ujbl7oi_2zOJ-x9GYhO6wACnpcq-M_ezUwRshh5zxuiO5TEYMYw=w2400",
+    imageUrl: "https://i.imgur.com/cG5zej4.png",
     imageCap: "Simple Calculator",
     appUrl: "https://takao21.github.io/ng-calculator/",
     sourceUrl: "https://github.com/Takao21/ng-calculator"
